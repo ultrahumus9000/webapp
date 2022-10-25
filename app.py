ce1e21468ec214812e82a999bf1c86c06b3c240e
Linlin Li <81304801+ultrahumus9000@users.noreply.github.com>
2022-10-25 18:51:02 +0100

    add html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 from datetime import datetime
 
 app = Flask(__name__)
@@ -7,19 +7,19 @@ counter = 0
 
 @app.route("/")
 def initialStage():
-    return"welcome to my first flask app"
+    return render_template("welcome.html")
 @app.route("/hi")
 def welcome():
     global counter
     counter+=1
     print("i am here "+ str(counter) + " times")
     return "you have viewed this for " + str(counter) + " times"
-welcome()
-print("1")
-welcome()
-print("2")
-welcome()
-print("3")
+# welcome()
+# print("1")
+# welcome()
+# print("2")
+# welcome()
+# print("3")
 @app.route("/time")
 
 def timeStamp():
@@ -27,7 +27,7 @@ def timeStamp():
 
 #this is to listen port and call welcome
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=8080)
+    app.run(debug=False, host='0.0.0.0',port=8080)
 
 
 
