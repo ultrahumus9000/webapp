1a2750294aff7d574b1e5a4abc7afa57a66a8a84
Linlin Li <81304801+ultrahumus9000@users.noreply.github.com>
2022-10-25 15:15:20 +0100

    add listener

--- flash.py
+++ flash.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
+
+@app.route("/")
+
+def welcome():
+    return "welcome to my first flask app"
 
 @app.route("/hello")
 
 def welcome():
-    return "welcome to my first flask app haha"
+    return "welcome to my first flask app"
 
+#this is to listen port and call welcome
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=8080)
