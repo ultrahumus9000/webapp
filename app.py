785cc8c280fe5e131c85f4313a6f82e6ee7fac28
Linlin Li <81304801+ultrahumus9000@users.noreply.github.com>
2022-10-26 23:43:59 +0100

    add post and new content to db

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, request, redirect, url_for
 from datetime import datetime
-from templates.model import  db
+from templates.model import db, save_db
 
 app = Flask(__name__)
 
 counter = 0
 
+
 @app.route("/")
 def initialStage():
     return render_template("welcome.html", message="LINLIN")
+
+
 @app.route("/hi")
 def welcome():
     global counter
-    counter+=1
-    print("i am here "+ str(counter) + " times")
+    counter += 1
+    print("i am here " + str(counter) + " times")
     return "you have viewed this for " + str(counter) + " times"
+
+
 # welcome()
 # print("1")
 # welcome()
@@ -25,20 +30,28 @@ def welcome():
 def timeStamp():
     return "welcome to this time" + str(datetime.today())
 
+
 @app.route("/card/<int:index>")
 def card_view(index):
     try:
         card = db[index]
-        return render_template("card.html",card=card, index=index)
+        return render_template("card.html", card=card, index=index)
     except IndexError:
         abort(404)
-#this is to listen port and call welcome
+
+
+@app.route('/add_card', methods=["GET", "POST"])
+def add_card():
+    if request.method == 'POST':
+        card = {"question": request.form['question'], "answer": request.form['answer']}
+        db.append(card)
+        print("what is new db", db, type(db))
+        save_db()
+        return redirect(url_for('card_view', index=len(db) - 1))
+    else:
+        return render_template('form.html')
+
+
+# this is to listen port and call welcome
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0',port=8080)
-
-
-
-
-
-
-
+    app.run(debug=False, host='0.0.0.0', port=8080)
--- templates/model.py
+++ templates/model.py
@@ -1,7 +1,13 @@
 import json
 
+
 def load_db():
     with open('templates/mockdb.json') as f:
         return json.load(f)
+
+
+def save_db():
+    with open('templates/mockdb.json', 'w') as f:
+        return json.dump(db, f)
 
 db = load_db()
